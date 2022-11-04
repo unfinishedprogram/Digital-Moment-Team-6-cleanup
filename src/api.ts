397ac7db45b6dc0aa2714defd0929260426244b6
Noah Labrecque <noah.labrecque@gmail.com>
2022-11-04 15:21:40 -0400

    passing errors through to client

--- src/api.ts
+++ src/api.ts
@@ -1,3 +1,4 @@
+import { json } from "stream/consumers";
 import { DemoArgs, DemoReturn } from "../pages/api/demo";
 
 type EndpointHandler<D, T> = (args: D) => T;
@@ -11,11 +12,17 @@ export default class Api {
   private static baseURL = "http://localhost:3000/api"
 
   public static async makeRequest<R extends keyof IEndpoints, D extends Parameters<IEndpoints[R]>, T extends ReturnType<IEndpoints[R]>>(route: R, ...args: D): Promise<T> {
-    const res = await fetch(`${this.baseURL}/${route}`, {
-      method: 'POST',
-      body: JSON.stringify(args)
-    });
+    return new Promise(async (res, rej) => {
+      const response = await fetch(`${this.baseURL}/${route}`, {
+        method: 'POST',
+        body: JSON.stringify(args)
+      });
 
-    return res.json()
+      if (response.status != 200) {
+        res(response.json());
+      } else {
+        rej(response.statusText);
+      }
+    });
   }
 }
