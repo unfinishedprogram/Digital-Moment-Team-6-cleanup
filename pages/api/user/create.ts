a6e94d8a44588d27098e4723346271235b2249cf
Noah Labrecque <noah.labrecque@gmail.com>
2022-11-04 15:50:10 -0400

    draft create user endpoint

--- pages/api/user/create.ts
+++ pages/api/user/create.ts
@@ -1,23 +1,23 @@
+import { User } from "pocketbase";
 import { TypedEndpoint } from "../../../src/lib/types/request";
+import pocketbase from "../../../src/pocketbase";
 
 export interface ICreateUserArgs {
-  username: string,
+  email: string,
   password: string,
 }
 
-export interface ICreateUserResponse {
-  status: "error" | "success",
-  message: string,
-}
-
-
-const handler: TypedEndpoint<ICreateUserArgs, ICreateUserResponse> = (req, res) => {
-  // Duplicate username
-  // Invalid password
-  res.status(200).json({
-    status: "error",
-    message: ""
-  });
+const handler: TypedEndpoint<ICreateUserArgs, User> = async (req, res) => {
+  try {
+    const user = await pocketbase.client.users.create({
+      email: req.body.email,
+      password: req.body.password,
+      passwordConfirm: req.body.password,
+    })
+    res.status(200).json(user);
+  } catch {
+    res.status(400);
+  }
 }
 
 export default handler;
