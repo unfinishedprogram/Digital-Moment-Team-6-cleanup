cade9dcebfcfd253fcda91cb751bd68a5325d014
Vinuyan Sivakolunthu <vinuyan.sivakolunthu@dawsoncollege.qc.ca>
2022-11-05 11:33:56 -0400

    added french translation

--- pages/register.tsx
+++ pages/register.tsx
@@ -12,8 +12,18 @@ let strings = new LocalizedStrings({
     repeat: "Repeat Password",
     age: "What age group are you in?",
     location: "Where are you from ?",
+  },
+  fr:{
+    username: "Nom d'utilisateur",
+    email: "Adresse Électronique",
+    password: "Mot de passe",
+    repeat: "Répéter votre mot de passe",
+    age: "Quel groupe d'âge fais-tu partie ?",
+    location: "D'où viens-tu",
+
   }
 })
+strings.setLanguage('fr')
 
 export default function Register() {
   let emailElement = React.useRef<HTMLInputElement>(null);
