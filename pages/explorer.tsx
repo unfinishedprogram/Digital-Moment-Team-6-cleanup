fbfc4b98972f812f677d8bc09261e30a17c9f0bb
Marian <marian.hristov@dawsoncollege.qc.ca>
2022-11-05 19:00:24 -0400

    Added post and tag components, having some issues pushing for gui to take a look

--- pages/explorer.tsx
+++ pages/explorer.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import Post from '../src/components/general/post';
+import { PostsRecord } from '../src/lib/types/pocket';
 
 export default function Explorer() {
+  let posts: Array<PostsRecord> = []
   return (<>
     <div className={'post-of-the-day'}>
       <h1>Post of the day</h1>
@@ -8,17 +11,8 @@ export default function Explorer() {
     <div className={'map'}>
       <h1>Map</h1>
     </div>
-    <div className={'post'}>
-      <h1>Random topic</h1>
-    </div>
-    <div className={'post'}>
-      <h1>Random topic</h1>
-    </div>
-    <div className={'post'}>
-      <h1>Random topic</h1>
-    </div>
-    <div className={'post'}>
-      <h1>Random topic</h1>
-    </div>
+    {posts.map(postObject => {
+      <Post post={postObject} />
+    })}
   </>);
 }
