80e6a59682ed0a20d52b2202f71f7379ff5c9a82
Noah Labrecque <noah.labrecque@gmail.com>
2022-11-04 21:06:44 -0400

    better seperation of components

--- pages/login.tsx
+++ pages/login.tsx
@@ -4,15 +4,6 @@ import styles from '../styles/Home.module.scss'
 export default function Login() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Title</title>
-        <meta name="description" content="Login" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1>Login</h1>
-      </main>
-    </div>
+    </div >
   )
 }
--- src/components/general/component-example.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default class ExampleComponent extends React.Component {
-  render() {
-    return <h1>Example component</h1>;
-  }
-}
