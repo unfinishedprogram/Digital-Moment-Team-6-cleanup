406772bd859bbc112e2ba5ec53dff4be8d423f57
Noah Labrecque <noah.labrecque@gmail.com>
2022-11-06 14:44:26 -0500

    Revert "some fixes"
    This reverts commit 9309613c19d57416ebbb60a82e4863b5315b924c.

--- pages/api/user/create.ts
+++ pages/api/user/create.ts
@@ -1,5 +1,5 @@
 import { User } from "pocketbase";
-import { TypedGetEndpoint } from "../../../src/lib/types/request";
+import { TypedEndpoint } from "../../../src/lib/types/request";
 import pocketbase from "../../../src/pocketbase";
 
 export interface ICreateUserArgs {
@@ -7,18 +7,18 @@ export interface ICreateUserArgs {
   password: string,
 }
 
-// const handler: TypedGetEndpoint<ICreateUserArgs, User> = async (req, res) => {
-//   // const data = {
-//   //   email: req.body.email,
-//   //   password: req.body.password,
-//   //   passwordConfirm: req.body.password,
-//   // }
+const handler: TypedEndpoint<ICreateUserArgs, User> = async (req, res) => {
+  const data = {
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.password,
+  }
 
-//   // pocketbase.client.users.create(data).then(data => {
-//   //   res.status(200).json(data);
-//   // }).catch(error => {
-//   //   res.status(400).send(error.data);
-//   // })
-// }
+  pocketbase.client.users.create(data).then(data => {
+    res.status(200).json(data);
+  }).catch(error => {
+    res.status(400).send(error.data);
+  })
+}
 
-// export default handler;
+export default handler;
