dbc3129dd9ae16591360b682f9869c89d2e3e3bf
nenikitov <nenikitov@mail.ru>
2022-11-06 15:52:07 -0500

    Made get profile return objects instead of ids

--- pages/api/user/get-user-profile.ts
+++ pages/api/user/get-user-profile.ts
@@ -1,6 +1,8 @@
 import { TypedGetEndpoint } from "../../../src/lib/types/request";
 import pocketbase from '../../../src/pocketbase';
 import { Profile } from "../../../src/lib/types/fullPocketTypes";
+import { getTags } from "../helper";
+import { RecordIdString } from "../../../src/lib/types/pocket";
 
 export type GetUserQueryParams = {
   userId: string;
@@ -19,7 +21,18 @@ const handler: TypedGetEndpoint<GetUserQueryParams, GetUserReturnParams> = async
   try {
     const userRecord = await pocketBaseConnection.users.getOne(userId);
     const profileRecord = await pocketBaseInstance.getOne("profiles", userRecord.profile!.id);
-    res.status(200).json(profileRecord as any);
+
+    const {age_group, preferences, location, ...response} = {...profileRecord};
+    const responseAgeGroup = await pocketBaseInstance.getOne("age_groups", profileRecord.age_group);
+    const responsePreferences = await getTags(profileRecord.preferences as unknown as RecordIdString[]);
+    const responseLocation = await pocketBaseInstance.getOne("tags", profileRecord.location);
+
+    res.status(200).json({
+      "age_group": responseAgeGroup,
+      "preferences": responsePreferences,
+      "location": responseLocation,
+      ...response
+    });
   } catch (error) {
     res.status(404).json(undefined);
   }
