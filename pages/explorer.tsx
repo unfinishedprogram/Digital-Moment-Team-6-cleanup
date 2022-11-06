8c17997024879e8dbb617d037617ae866b1b10ab
Marian <marian.hristov@dawsoncollege.qc.ca>
2022-11-06 12:20:45 -0500

    Add posts with useState

--- pages/explorer.tsx
+++ pages/explorer.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PostComponent from '../src/components/general/post';
 import { Post } from '../src/lib/types/fullPocketTypes';
 
 export default function Explorer() {
   // This array needs to be populated
-  let posts: Post[] = [];
+  let [posts, setPosts] = useState<Post[]>([])
   return (<>
     <div className={'post-of-the-day'}>
       <h1>Post of the day</h1>
