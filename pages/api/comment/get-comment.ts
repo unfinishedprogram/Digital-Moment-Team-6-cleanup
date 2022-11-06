8af5f32d8b9f5aaf1c4458c8114b05da4fbba209
nenikitov <nenikitov@mail.ru>
2022-11-05 20:55:15 -0400

    Added get comments api

---
+++ pages/api/comment/get-comment.ts
@@ -0,0 +1,33 @@
+
+import { TypedGetEndpoint } from "../../../src/lib/types/request";
+import { Comment } from "../../../src/lib/types/fullPocketTypes";
+import pocketbase from '../../../src/pocketbase';
+
+export type GetPostQueryParams = {
+  postId: string;
+}
+
+export type GetPostReturnParams = Comment | undefined;
+
+const handler: TypedGetEndpoint<GetPostQueryParams, Comment | undefined> = async (req, res) => {
+  const { postId: commentId } = req.query;
+  if (commentId == undefined) {
+    res.status(400);
+    return;
+  }
+  const pocketBaseInstance = pocketbase;
+
+  try {
+    const postRecord = await pocketBaseInstance.getOne("comments", commentId);
+    const postInfo = await pocketBaseInstance.getOne("post_infos", postRecord.post_info);
+
+    const {post_info, ...response} = {...postRecord, ...postInfo};
+
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(404).send(undefined);
+  }
+
+}
+
+export default handler;
