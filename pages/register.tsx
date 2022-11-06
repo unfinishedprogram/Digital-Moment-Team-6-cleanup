0e9de5c5e9aaa53cd5699c404fa2a0dacdc31fe2
Vinuyan Sivakolunthu <vinuyan.sivakolunthu@dawsoncollege.qc.ca>
2022-11-06 01:13:01 -0400

    refactored register form to use <Formik> tag

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,17 +1,17 @@
 import Head from 'next/head'
-import React from 'react';
+import * as React from 'react';
 import Api from '../src/api';
 import styles from '../styles/Home.module.scss'
 import input_styles from "../styles/input.module.scss"
 import  dropdown_styles from "../styles/dropdownSelect.module.scss"
-import {useFormik} from 'formik';
+import {useFormik, Formik, FormikHelpers, FormikProps, Form, Field, FieldProps} from 'formik';
 import { strings } from '../src/localization/localization-register'
 import ButtonConfirm from '../src/components/general/button/button-confirm';
 import Link from 'next/link';
 import TagSelector from "../src/components/general/tagSelector"
 import Select from "react-select"
 let lang =['English', 'French', 'Spanish']
-let preferences = ["politics", "racism", "war"]
+let pref = ["politics", "racism", "war"]
 const ageOptions = [
   { value: '8-10', label: '8-10'  },
   { value: '11-13', label: '11-13' },
@@ -19,57 +19,29 @@ const ageOptions = [
   { value: '16-17', label: '16-17' },
 ]
 
-export default function Register() {
-  const validate = (values: {
-    password: any;
-    repeat: any;
-    email: any;
-    }) =>{  
-    const passwordRegex = RegExp("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$");
-    const emailRegex = RegExp("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
-    const errors : typeof values  = {
-      password: undefined,
-      repeat: undefined,
-      email: undefined,
-    };
-    console.log(passwordRegex.test(values.password));
-    if(values.password && !passwordRegex.test(values.password)){
-      errors.password = strings.passwordError;
-    }    
-    if(values.repeat  &&  values.password != values.repeat ){
-      errors.repeat = strings.repeatError;
-    }
-    if(!emailRegex.test(values.email)){
-      errors.email = strings.emailError;
-    }
-    return errors
+interface FormValues{
+  username: string;
+  password: string;
+  repeat: string;
+  email: string;
+  age: string;
+  location: string;
+  preferences: string[];
+  languages: string[];
+}
+export default function MyApp(){
+  const initialValues: FormValues = {
+    username: "",
+    email: "",
+    password: "",
+    repeat: "",
+    age: "",
+    location: "",
+    preferences: [""],
+    languages: [""],
   }
 
-  const formik = useFormik({
-    initialValues: {
-      username: "",
-      email: "",
-      password: "",
-      repeat: "",
-      age: "",
-      location: "",
-      preferences: [],
-      languages: [],
-    },
-    validate,
-    onSubmit: values=>{
-      Api.makePostRequest("user/create",{
-        email: formik.values.email,
-        password: formik.values.password,
-        location: formik.values.location,
-        age: formik.values.age,
-        preferences: formik.values.preferences,
-        languages: formik.values.languages,
-      })
-    }
-  });
-
-  return (
+  return(
     <div className={styles.container}>
       <Head>
         <title>Title</title>
@@ -79,74 +51,142 @@ export default function Register() {
 
       <main className={styles.main}>
         <h1>Register</h1>
-        <form onSubmit={formik.handleSubmit} >
-          <input 
-            placeholder={strings.username} 
-            className={input_styles['text-input']} 
-            type="text" 
-            id="username"
-            name="username" 
-            onChange={formik.handleChange} 
-            value={formik.values.username} 
-            required 
-          />
-          {formik.errors.username ? <label htmlFor='username'>{formik.errors.username}</label>: null}
-          <input 
-            placeholder={strings.email}
-            className={input_styles['text-input']} 
-            type="text"
-            id="email"
-            name="email" 
-            onChange={formik.handleChange} 
-            value={formik.values.email} 
-            required 
-          />
-          {formik.errors.email ? <label htmlFor='email'>{formik.errors.email}</label>: null}
-          <input 
-            placeholder={strings.password}
-            className={input_styles['text-input']} 
-            type="password"
-            id="password"
-            name="password" 
-            onChange={formik.handleChange} 
-            value={formik.values.password} 
-            required 
-          />
-          {formik.errors.password ? <label htmlFor='password'>{formik.errors.password}</label>: null}
-          <input 
-            placeholder={strings.repeat}
-            className={input_styles['text-input']} 
-            type="password"
-            id="repeat"
-            name="repeat" 
-            onChange={formik.handleChange} 
-            value={formik.values.repeat} 
-            required 
-          />
-          {formik.errors.repeat ? <label htmlFor='repeat'>{formik.errors.repeat}</label>: null}
-          <Select 
-            options={ageOptions}
-            placeholder={strings.age} 
-            onChange={formik.handleChange} 
-            className={dropdown_styles['dropdown']}
-            id="age" 
-            name="age" 
-          />
-          <input 
-            placeholder={strings.location} 
-            className={input_styles['text-input']} 
-            type="text" 
-            id="location" 
-            name="location" 
-            onChange={formik.handleChange} 
-            value={formik.values.location}
-            required
-          />
-          {formik.errors.location ? <label htmlFor='location'>{formik.errors.location}</label>: null}
-          <TagSelector tags={lang} placeholder={strings.languages}></TagSelector>
-          <TagSelector tags={preferences} placeholder={strings.preferences}></TagSelector>
-          <ButtonConfirm>{strings.register}</ButtonConfirm>
-        </form>
+        <Formik
+          initialValues={initialValues}
+          // validate = {values =>{
+          //   const passwordRegex = RegExp("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$");
+          //   const emailRegex = RegExp("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
+          //   const errors :{
+          //     password: any;
+          //     repeat: any;
+          //     email: any;
+          //     preferences: any;
+          //     languages: any;
+          //   } = {
+          //     password: undefined,
+          //     repeat: undefined,
+          //     email: undefined,
+          //     preferences: undefined,
+          //     languages: undefined,
+          //   };
+          //   if(values.password && !passwordRegex.test(values.password)){
+          //     errors.password = strings.passwordError;
+          //   }    
+          //   if(values.repeat  &&  values.password != values.repeat ){
+          //     errors.repeat = strings.repeatError;
+          //   }
+          //   if(!emailRegex.test(values.email)){
+          //     errors.email = strings.emailError;
+          //   }
+          //   if(values.preferences.length == 0){
+          //     errors.preferences = strings.requiredError;
+          //   }
+          //   if(values.languages.length == 0){      
+          //     errors.languages = strings.requiredError;
+          //   }
+          // }}
+          onSubmit={(values, actions) => {
+            console.log({values, actions})
+            actions.setSubmitting(false)
+          }}
+        >
+          {({handleSubmit, handleChange, values, errors}) =>(
+            <Form onSubmit={(e) => {
+              e.preventDefault()
+              handleSubmit(e)}}
+            >
+              <input 
+                placeholder={strings.username} 
+                className={input_styles['text-input']} 
+                type="text" 
+                id="username"
+                name="username" 
+                onChange={handleChange} 
+                value={values.username} 
+                required 
+              />
+              {errors.username ? <label htmlFor='username'>{errors.username}</label>: null}
+              <input 
+                placeholder={strings.email}
+                className={input_styles['text-input']} 
+                type="text"
+                id="email"
+                name="email" 
+                onChange={handleChange} 
+                value={values.email} 
+                required 
+              />
+              {errors.email ? <label htmlFor='email'>{errors.email}</label>: null}
+              <input 
+                placeholder={strings.password}
+                className={input_styles['text-input']} 
+                type="password"
+                id="password"
+                name="password" 
+                onChange={handleChange} 
+                value={values.password} 
+                required 
+              />
+              {errors.password ? <label htmlFor='password'>{errors.password}</label>: null}
+              <input 
+                placeholder={strings.repeat}
+                className={input_styles['text-input']} 
+                type="password"
+                id="repeat"
+                name="repeat" 
+                onChange={handleChange} 
+                value={values.repeat} 
+                required 
+              />
+              {errors.repeat ? <label htmlFor='repeat'>{errors.repeat}</label>: null}
+              <Select 
+                options={ageOptions}
+                placeholder={strings.age} 
+                onChange={ e =>{values.age =  e!["value"]}} 
+                className={dropdown_styles.dropdown}
+                instanceId="age" 
+                name="age" 
+              />
+              <input 
+                placeholder={strings.location} 
+                className={input_styles['text-input']} 
+                type="text" 
+                id="location" 
+                name="location" 
+                onChange={handleChange} 
+                value={values.location}
+                required
+              />
+              {errors.location ? <label htmlFor='location'>{errors.location}</label>: null}
+              <TagSelector 
+                tags={lang} 
+                placeholder={strings.languages}
+                instanceId="languages" 
+                onChange={e => {
+                  let langs: string[] = []
+                  e.forEach(el=> langs.push(el.value))
+                  values.languages = langs
+                }
+                }
+              />
+              {errors.languages ? <label htmlFor='languages'>{errors.languages}</label>: null}
+              <TagSelector 
+                tags={pref}
+                placeholder={strings.preferences}
+                instanceId="pref" 
+                name='pref'
+                onChange={e => {
+                  let prefs: string[] = []
+                  e.forEach(el=> prefs.push(el.value))
+                  values.preferences = prefs
+                }  
+                }
+              />
+              {errors.preferences ? <label htmlFor='pref'>{errors.preferences}</label>: null}
+              <ButtonConfirm type='submit'>{strings.register}</ButtonConfirm>
+            </Form>
+          )}
+        </Formik>
         <h4>Already have an account? <Link href="/login">Login</Link></h4>
       </main>
     </div>
