9b976993340663fdab167ec67353550d2580f6a0
S4more <guimsc@hotmail.com>
2022-11-06 23:18:21 -0500

    ESLint.

--- src/components/admin/post-validation.tsx
+++ src/components/admin/post-validation.tsx
@@ -11,25 +11,25 @@ export default function PostValidationMenu(prop: PostValidationMenuProps) {
   const [selected, setSelected] = useState<number>();
 
   return (
-  <div className={style.postReview}>
+    <div className={style.postReview}>
       {posts.map((post, index) => (
         <>
-        <div className={style.row} onClick={() => setSelected(index)}>
-          <p> Author: { post.author } </p>
-          <p> Title: { post.title } </p>
-        </div>
-        {index == selected && (
-          <div className={style.opened}>
-            <p> { post.body } </p>
-            <div className={style.removeRow}>
+          <div className={style.row} onClick={() => setSelected(index)}>
+            <p> Author: { post.author } </p>
+            <p> Title: { post.title } </p>
+          </div>
+          {index == selected && (
+            <div className={style.opened}>
+              <p> { post.body } </p>
+              <div className={style.removeRow}>
                 <button className={`${style.button} ${style.green}`}> Approve </button>
                 <button className={`${style.button}`}> Reject </button>
+              </div>
             </div>
-          </div>
-        )}
+          )}
         </>
       ))}
-  </div>
+    </div>
   )
 
 }
