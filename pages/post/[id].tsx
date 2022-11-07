4149c93d3bfaf487e31c614285f4796aa2c055bb
Strange4 <>
2022-11-06 19:28:18 -0500

    why does it work?

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,8 +1,25 @@
 import Head from 'next/head'
-import CommentComponent from '../../src/components/post/comment'
+import {CommentComponent} from '../../src/components/post/comment'
 import styles from '../../styles/Home.module.scss'
 
+interface IComment {
+  text:string, 
+  children:IComment[]
+}
+
+
+
 export default function Post() {
+  const demoComment = {
+    text:"this is nice",
+    children: [
+      {
+        text:"this is nicer",
+        children:[]
+      }
+    ]
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,7 +30,7 @@ export default function Post() {
 
       <main className={styles.main}>
         <h1>Post</h1>
-        <CommentComponent/>
+        <CommentComponent commentChildren={demoComment.children} text={demoComment.text}/>
       </main>
     </div>
   )
--- src/components/post/comment.tsx
+++ src/components/post/comment.tsx
@@ -2,20 +2,32 @@ import React from "react";
 import Image from "next/image";
 import styles from'../../../styles/comment.module.scss'
 
-export default class CommentComponent extends React.Component {
-  render(): React.ReactNode {
-    return (
-      <div className={styles.hor}>
-        <div className={styles.vert}>
-          <Image src="/img/avatar.svg" width="50" height="50" alt="avatar-placeholder" />
-          <i className={styles.thread}></i>
-        </div>
-        <div>
-          <h1 className={styles.title}>Title</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus minus iste a accusantium tempore, dolorem perspiciatis delectus dolores quasi repellendus totam sunt ex? Amet explicabo, at nulla exercitationem reiciendis quaerat?</p>
-        </div>
-      </div>
-    );
 
-  }
+interface IComment {
+  text:string, 
+  children:IComment[]
+}
+
+
+interface ICommentProps extends React.PropsWithChildren {
+  text:string, 
+  commentChildren:IComment[]
+}
+
+export const CommentComponent: React.FunctionComponent<ICommentProps> = props => {
+  const children = props.children;
+  return (
+    <div className={styles.hor}>
+      <div className={styles.vert}>
+        <Image src="/img/avatar.svg" width="50" height="50" alt="avatar-placeholder" />
+        <i className={styles.thread}></i>
+      </div>
+      <div>
+        <h1 className={styles.title}>{props.text}</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus minus iste a accusantium tempore, dolorem perspiciatis delectus dolores quasi repellendus totam sunt ex? Amet explicabo, at nulla exercitationem reiciendis quaerat?</p>
+        {props.commentChildren.map((child, index) => <CommentComponent key={index} text={child.text} commentChildren={child.children} />)}
+      </div>
+    </div>
+  );
+
 }
