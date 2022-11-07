07ba82978dd04651bbca3dac75d15d783bd929c7
S4more <guimsc@hotmail.com>
2022-11-06 21:12:24 -0500

    Put everything into a compononet.

---
+++ src/components/admin/admin.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import style from "../../../styles/admin.module.scss";
+import Footer, { PagesOptions } from "./footer";
+import TagAssociation from "./tag-row";
+const SDGS = [
+  "Poverty",
+  "Hunger",
+  "Healthy Lives",
+  "Inclusive and Quality education",
+  "Gender Equality",
+  "Access to water and sanitization",
+  "Affordable, reliable, sustainable energy for all",
+  "Economic growth and employment",
+]
+
+const TAGS = [
+  "Racism",
+  "Poverty",
+  "Immigration",
+  "Civil Rights",
+  "Gender Inequality",
+  "Health Care",
+  "Bullying",
+];
+
+export default function Admin() {
+  const [currentPage, setCurrentPage] = useState<PagesOptions>(PagesOptions.Stats);
+
+  return (
+    <div className={style.adminWrapper}>
+
+      {currentPage == PagesOptions.Stats && (
+        <div className={style.tagsWrapper}>
+          { TAGS.map((tag, index) =>
+            <TagAssociation sdgIssueList={SDGS} tagName={tag} occurrences={10} selectedSdgIssue={""} key={index}/>
+          ) }
+        </div>
+      )
+      }
+
+      <Footer selected={currentPage} onClick={(arg) => setCurrentPage(arg)}/>
+
+    </div>
+  )
+}
