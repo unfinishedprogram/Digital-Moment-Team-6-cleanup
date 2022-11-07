14730324b93c2a9e8679e70077ce7d3dc1be7a2d
Mathieu Laramée Pelosse <m.larameepelosse@gmail.com>
2022-11-06 21:17:16 -0500

    Display line graph for posts per day.

--- pages/admin/general.tsx
+++ pages/admin/general.tsx
@@ -19,11 +19,24 @@ interface IDonut {
   series: number[];
 }
 
+interface ILineData {
+  name: string;
+  data: number[];
+}
+
+interface ILineTitle {
+  text: string;
+}
+
 interface ILine {
   options: {
-
+    title: ILineTitle;
+    colors: string[];
   }
+  series: ILineData[];
 }
+
+const colorPalette: string[] = ['#00D8B6','#008FFB',  '#FEB019', '#FF4560', '#775DD0'];
 
 export default function adminGeneral() {
 
@@ -41,8 +54,35 @@ export default function adminGeneral() {
     series: [44, 55, 41, 17, 15]
   }
 
+  let line: ILine = {
+    options: {
+      title: { 
+        text: 'Posts per Day'
+      },
+      colors: colorPalette,
+    },
+    series: [
+      {
+        name: 'Amount of Challenge Posts',
+        data: [10, 20, 15, 2, 6, 21]
+      },
+      {
+        name: 'Amount of Idea Posts',
+        data: [5, 3, 14, 7, 2, 10]
+      }
+    ]
+  }
+
   return (
     <div className={styles.charts}>
+      <div>
+        <Chart
+          options={line.options}
+          series={line.series}
+          type="line"
+          width="500"
+        />
+      </div>
       <div>
         <Chart 
           options={donut.options} 
