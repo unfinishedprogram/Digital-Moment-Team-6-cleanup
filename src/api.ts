43fb9d97638e4d3df199183c8e663c8e3864d77c
nenikitov <nenikitov@mail.ru>
2022-11-06 23:32:15 -0500

    Integraing list endpoints

--- src/api.ts
+++ src/api.ts
@@ -7,6 +7,7 @@ import { AddCommentBodyParams, AddCommentReturnParams } from "../pages/api/comme
 import { GetCommentQueryParams, GetCommentReturnParams } from "../pages/api/comment/get-comment";
 import { GetTagQueryParams, GetTagReturnParams } from "../pages/api/tag/get-tag";
 import { GetPostCommentsQueryParams, GetPostCommentsReturnParams } from "../pages/api/post/get-post-comments";
+import { GetTagQueryParams, GetTagReturnParams } from "../pages/api/tag/get-tag";
 
 type EndpointHandler<D, T> = (args: D) => T;
 
@@ -22,6 +23,8 @@ interface IGetEndpoints {
   "comment/get-comment": EndpointHandler<GetCommentQueryParams, GetCommentReturnParams>
   "tag/get-tag": EndpointHandler<GetTagQueryParams, GetTagReturnParams>
   "post/get-post-comments": EndpointHandler<GetPostCommentsQueryParams, GetPostCommentsReturnParams>
+  "post-comments/get-post-comments": EndpointHandler<GetPostCommentsQueryParams, GetPostCommentsReturnParams>
+  "tag/get-tag": EndpointHandler<GetTagQueryParams, GetTagReturnParams>
 }
 
 
