76443c6f2f9a2320f9479d229ab4ef28a400d467
Noah Labrecque <noah.labrecque@gmail.com>
2022-11-07 14:14:09 -0500

    filtering

--- src/components/explorer.tsx
+++ src/components/explorer.tsx
@@ -5,11 +5,30 @@ import { Post, PostWithComments } from '../lib/types/fullPocketTypes';
 
 export interface IExplorerProps {
   posts: Post[],
+  filter: string[] | undefined;
+}
+
+interface ISortablePost {
+  post: Post,
+  overlap: number,
 }
 
 const Explorer: React.FunctionComponent<IExplorerProps> = props => {
+  const filter = (posts: Post[], tags?: string[]) => {
+    if (!tags) return posts;
+
+    let sortable: ISortablePost[] = posts.map(post => {
+      let overlap = post.tags.filter(t => t.name in tags).length;
+      return { post, overlap };
+    });
+    return sortable.sort((a, b) => a.overlap - b.overlap).map(p => p.post);
+  }
+
   // This array needs to be populated with the API
-  return <>{props.posts.map((post, index) => <PostComponent post={post as PostWithComments} key={index} />)}</>
+  return <>{
+    filter(props.posts, props.filter).map((post, index) =>
+      <PostComponent post={post as PostWithComments} key={index} />
+    )}</>;
 }
 
 export default Explorer;
