359a039be5c12dcfac2d5cddd157756cf656680b
Noah Labrecque <noah.labrecque@gmail.com>
2022-11-06 20:40:07 -0500

    oops

--- src/components/general/tag.tsx
+++ src/components/general/tag.tsx
@@ -1,15 +1,9 @@
+
 import React from "react";
 import styles from "../../../styles/post.module.scss"
 import { TagsRecord } from "../../lib/types/pocket";
 
-export default function TagComponent(props: { tag: TagsRecord }) {
-  let tag: TagsRecord = props.tag
-  return (
-    <>
-      {
-        tag && <div className={[styles.tag, styles[`tag-type-${tag.type}`]].join(" ")
-        }>{tag.name}</div>
-      }
-    </>
-  )
-}
+const TagComponent: React.FunctionComponent<{ tag: TagsRecord }> = props =>
+  <div className={[styles.tag, styles[`tag-type-${props.tag.type}`]].join(" ")}>{props.tag.name}</div>
+
+export default TagComponent;
